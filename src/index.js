d287aed2dae4b823f0728b85d6d115be3d576b62
siddharthsharma983 <gs7248543@gmail.com>
2026-03-17 13:47:34 +0530

    fix: render deployment issue

--- src/index.js
+++ src/index.js
@@ -1,51 +1,21 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
+
+// Load env first
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
-
+const PORT = process.env.PORT || 8000;
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
-
-
-
-
-
-
-
-
-
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-
-*/
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("❌ MONGO DB connection failed:", err);
+    process.exit(1); // important for render
+  });
